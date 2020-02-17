fe273aaa2c42ddcb17efdd6cb57b4884c8effb12
kin <kin.poon@concentra.co.uk>
2020-02-17 12:43:39 +0000

    Run update

--- GAIN/Run.py
+++ GAIN/Run.py
@@ -23,13 +23,13 @@ Discriminator=myDiscriminator(compositLayers([Dim*48,Dim*15,Dim*15,Dim*10,Dim*15
 # each GAN model could not be reused for another adversarial pair: 
 # https://github.com/tensorflow/tensorflow/issues/27120
 # Should use one single model once the issue is resolved.
+Model1=GAN(hyperParams={'p_miss':1/6, 'p_hint':1/6},optimizer=tf.keras.optimizers.Adam())
+Model2=GAN(logdir=Model1.logdir,hyperParams={'p_miss':1/6, 'p_miss':1/6},optimizer=tf.keras.optimizers.Adam(1e-4))
 #%% Run - Step 1
 # First train the discriminator against a random generator to increase its stability
 counter=0
-train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=200)
+train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=2000)
 test=iter(test)
-Model1=GAN(hyperParams={'p_miss':1/6},optimizer=tf.keras.optimizers.Adam())
-Model2=GAN(logdir=Model1.logdir,hyperParams={'p_miss':1/6},optimizer=tf.keras.optimizers.Adam(1e-4))
 for dat_train in tqdm(train):
     Model1.trainWithSteps(dat_train,randomGenerator,Discriminator)
     if(counter%20==0):
@@ -37,26 +37,26 @@ for dat_train in tqdm(train):
         Model1.performanceLog('<Random Generator>(test)',test.next(),randomGenerator,Discriminator)
 #%% Run - Step 2 (unrolled)
 # Then train the generator and discriminator with discriminator unrolling. 
-# counter=0
-# train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=400)
-# test=iter(test)
-# Model2.initialiseEpisodes(Discriminator,myDiscriminator)
-# for dat_train in tqdm(train):
-#     Model2.unrollDiscriminator(dat_train,Generator,Discriminator)
-#     Model2.trainGeneratorWithEpisodes(dat_train,Generator,Discriminator)
-#     if(counter%20==0):
-#         Model2.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
-#         Model2.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
+counter=0
+train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=400)
+test=iter(test)
+Model2.initialiseEpisodes(Discriminator,myDiscriminator)
+for dat_train in tqdm(train):
+    Model2.unrollDiscriminator(dat_train,Generator,Discriminator)
+    Model2.trainGeneratorWithEpisodes(dat_train,Generator,Discriminator)
+    if(counter%20==0):
+        Model2.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
+        Model2.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
 
 
 #%% Run - Step 2
 # Then train the discriminator against a train-able generator model without unrolling
-counter=0
-train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=1)
-test=iter(test)
-Model2=GAN(logdir=Model1.logdir,hyperParams={'p_miss':1/6},optimizer=tf.keras.optimizers.Adam(1e-4))
-for dat_train in tqdm(train):
-    Model1.trainWithSteps(dat_train,randomGenerator,Discriminator)
-    if(counter%20==0):
-        Model2.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
-        Model2.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
+# counter=0
+# train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=1)
+# test=iter(test)
+# Model2=GAN(logdir=Model1.logdir,hyperParams={'p_miss':1/6},optimizer=tf.keras.optimizers.Adam(1e-4))
+# for dat_train in tqdm(train):
+#     Model1.trainWithSteps(dat_train,randomGenerator,Discriminator)
+#     if(counter%20==0):
+#         Model2.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
+#         Model2.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
