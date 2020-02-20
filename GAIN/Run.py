24e0fd7bc80b457ee6edc6a2688814215233acf3
kin <kin.poon@concentra.co.uk>
2020-02-20 11:21:54 +0000

    restructure run

--- GAIN/Run.py
+++ GAIN/Run.py
@@ -23,49 +23,51 @@ Discriminator=myDiscriminator(compositLayers([Dim*10,Dim*10,Dim*10,Dim*10,Dim*10
 # each GAN model could not be reused for another adversarial pair: 
 # https://github.com/tensorflow/tensorflow/issues/27120
 # Should use one single model once the issue is resolved.
-Model1=GAN(hyperParams={'p_miss':1/6, 'p_hint':1/6},optimizer=tf.keras.optimizers.Adam())
-Model2=GAN(logdir=Model1.logdir,hyperParams={'p_miss':1/6, 'p_miss':1/6, 'alpha':0.1, 'episode_num':10 },optimizer=tf.keras.optimizers.Adam())
+Model1=GAN(hyperParams={'p_miss':0.5, 'p_hint':0.5},optimizer=tf.keras.optimizers.Adam())
+Model2=GAN(summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5, 'p_hint':0.5},optimizer=tf.keras.optimizers.Adam())
+Model3=GAN(summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5, 'p_miss':0.5, 'alpha':0.1, 'episode_num':10 },optimizer=tf.keras.optimizers.Adam(1e-4))
 #%% Run - Step 1
 # First train the discriminator against a random generator to increase its stability
 counter=0
 train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=2000)
 test=iter(test)
 for dat_train in tqdm(train):
-    Model1.trainWithSteps(dat_train,randomGenerator,Discriminator)
+    Model1.trainWithSteps(dat_train,randomGenerator,Discriminator,steps=False)
     if(counter%20==0):
         Model1.performanceLog('<Random Generator>(train)',dat_train,randomGenerator,Discriminator)
         Model1.performanceLog('<Random Generator>(test)',test.next(),randomGenerator,Discriminator)    
-    if(counter%100==0):
-        Discriminator.save(Model1.logdir+'\DiscriminatorS1E{}'.format(counter))
-    counter+=1
-#%% Run - Step 2 (unrolled)
-# Then train the generator and discriminator with discriminator unrolling. 
+    counter+=1    
+    
+Discriminator.save(Model1.logdir+'Models\DiscriminatorS1E{}'.format(counter))
+#%% Run - Step 2
+# Then train the discriminator against a train-able generator model without unrolling
 counter=0
 train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=5000)
 test=iter(test)
-Model2.initialiseEpisodes(Discriminator,myDiscriminator)
 for dat_train in tqdm(train):
-    Model2.trainDiscriminator(dat_train,Generator,Discriminator)
-    Model2.trainDiscriminator(dat_train,Generator,Discriminator)
-    Model2.trainDiscriminator(dat_train,Generator,Discriminator)
-    Model2.unrollDiscriminator(dat_train,Generator,Discriminator)
-    Model2.trainGeneratorWithEpisodes(dat_train,Generator,Discriminator)
+    Model2.trainWithSteps(dat_train,Generator,Discriminator)
     if(counter%20==0):
         Model2.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
         Model2.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
     counter+=1
-Generator.save(Model2.logdir+'\GeneratorS2E{}'.format(counter))
-Discriminator.save(Model2.logdir+'\DiscriminatorS2E{}'.format(counter))
+    if counter>3000 and counter%100==0:
+        Generator.save(Model2.logdir+'\Models\GeneratorS2E{}'.format(counter))
+        Discriminator.save(Model2.logdir+'\Models\DiscriminatorS2E{}'.format(counter))
 
+#%% Run - Step 3 (unrolled)
+# Then train the generator and discriminator with discriminator unrolling. 
+counter=0
+train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=1000)
+test=iter(test)
+Model3.initialiseEpisodes(Discriminator,myDiscriminator)
+for dat_train in tqdm(train):
+    Model3.unrollDiscriminator(dat_train,Generator,Discriminator)
+    Model3.trainGeneratorWithEpisodes(dat_train,Generator,Discriminator)
+    if(counter%20==0):
+        Model3.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
+        Model3.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
+    counter+=1
+Generator.save(Model3.logdir+'\GeneratorS3E{}'.format(counter))
+Discriminator.save(Model3.logdir+'\DiscriminatorS3E{}'.format(counter))
 
-#%% Run - Step 2
-# Then train the discriminator against a train-able generator model without unrolling
-# counter=0
-# train,test=Data.getPipeLine(train_rate=0.8,batch_ratio=1,repeat=1)
-# test=iter(test)
-# Model2=GAN(logdir=Model1.logdir,hyperParams={'p_miss':1/6},optimizer=tf.keras.optimizers.Adam(1e-4))
-# for dat_train in tqdm(train):
-#     Model1.trainWithSteps(dat_train,randomGenerator,Discriminator)
-#     if(counter%20==0):
-#         Model2.performanceLog('<Generator>(train)',dat_train,Generator,Discriminator)
-#         Model2.performanceLog('<Generator>(test)',test.next(),Generator,Discriminator)
+# %%
