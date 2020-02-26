f7895db0b416e9f545e95b7efb61aa671244451f
kin <kin.poon@concentra.co.uk>
2020-02-26 22:54:51 +0000

    in-out memoisers

--- GAIN/components/Memorisor.py
+++ GAIN/components/Memorisor.py
@@ -1,13 +1,13 @@
 #%%
 from tensorflow import concat, split, identity_n, squeeze, TensorShape,Variable,float32
 
-class MemoryReplay():
+class MemoiseIn():
     def __init__(self,func,size):
         self.size=size
         self.func=func
         self.pointer=0
     def initiateMemory(self,*inputs):
-        self.memory=[[identity_n(input) for i in range(0,len(inputs))] for j in range(0,self.size)]
+        self.memory=[identity_n(inputs) for j in range(0,self.size)]
     def __pointNext(self):
         self.pointer=(self.pointer+1)%self.size
     
@@ -17,7 +17,7 @@ class MemoryReplay():
         inputs=split(self.memory,axis=1,num_or_size_splits=len(args))
         return self.func(*[squeeze(x) for x in inputs])
 #%%
-class Memory_Output():
+class MemoiseOut():
     def __init__(self,func,size):
         self.size=size
         self.func=func
@@ -25,18 +25,15 @@ class Memory_Output():
     
     def initiateMemory(self,*inputs):
         outputs=self.func(*inputs)
-        self.memory_out=[identity_n(outputs) for i in range(self.size)]
-        return outputs
+        self.memory=[identity_n(outputs) for i in range(self.size)]
     
     def __pointNext(self):
         self.pointer=(self.pointer+1)%self.size
     
     def __call__(self,*args):
-        self.memory[self.pointer]=identity_n(args)
+        output=self.func(*args)
+        self.memory[self.pointer]=identity_n(output)
         self.__pointNext()
-        inputs=split(self.memory,axis=1,num_or_size_splits=len(args))
-        output=self.func(*[squeeze(x) for x in inputs])
-        self.memory_out[self.pointer]=identity_n(output)
         return output
 
 # %%
