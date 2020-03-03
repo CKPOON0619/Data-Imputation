eced9c65497b7ddb5ed6830711f2a0e8c50a1326
kin <kin.poon@concentra.co.uk>
2020-03-03 17:48:06 +0000

    adding replay for meorisor

--- GAIN/components/Memorisor.py
+++ GAIN/components/Memorisor.py
@@ -3,21 +3,27 @@ from tensorflow import concat, split, identity_n, squeeze, TensorShape,Variable,
 from tensorflow.keras import Model
 
 class MemoiseIn(Model):
-    def __init__(self,func,size):
+    def __init__(self,model,size):
         super(MemoiseIn, self).__init__()
         self.size=size
-        self.func=func
+        self.model=model
         self.pointer=0
     def initiateMemory(self,*inputs):
         self.memory=[identity_n(inputs) for j in range(0,self.size)]
     def __pointNext(self):
         self.pointer=(self.pointer+1)%self.size
-    
     def call(self,*args):
+        self.argLen=len(args)
         self.memory[self.pointer]=identity_n(args)
         self.__pointNext()
-        inputs=split(self.memory,axis=1,num_or_size_splits=len(args))
-        return self.func(*[squeeze(x) for x in inputs])
+        return self.model(*args)
+    def replay(self):
+        inputs=split(self.memory,axis=1,num_or_size_splits=self.argLen)
+        return self.model(*[squeeze(x) for x in inputs])
+    def recall(self):
+        return split(self.memory,axis=1,num_or_size_splits=self.argLen)
+        
+    
 #%%
 class MemoiseOut(Model):
     def __init__(self,model,size):
