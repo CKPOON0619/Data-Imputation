857bc401aca8ec5ac0ee2567697dec0fd8567b48
kin <kin.poon@concentra.co.uk>
2020-04-14 12:13:42 +0100

    update to Run

--- GAIN/Run.py
+++ GAIN/Run.py
@@ -26,11 +26,11 @@ Discriminator=myDiscriminator(compositLayers([Dim*10,Dim*10,Dim*10,Dim*20,Dim*20
 # Should use one single model once the issue is resolved.
 Model1=GAN(hyperParams={'p_miss':0.5},optimizer=tf.keras.optimizers.Adam())
 Model2=GAN(summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5},optimizer=tf.keras.optimizers.Adam())
-Model3=GAN(summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5, 'alpha':0.1, 'episode_num':10 },optimizer=tf.keras.optimizers.Adam(1e-4))
+Model3=GAN(summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5, 'alpha':0.1, 'episode_num':10 },optimizer=tf.keras.optimizers.Adam(1e-3))
 #%% Run - Step 1
 # First train the discriminator against a random generator to increase its stability
 counter=0
-train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=1,repeat=5)
+train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=1)
 test=iter(test)
 for dat_train,[mask,hintMask,hints] in tqdm(train):
     Model1.trainWithSteps(dat_train,mask,hints,randomGenerator,Discriminator,steps=False)
@@ -40,58 +40,43 @@ for dat_train,[mask,hintMask,hints] in tqdm(train):
         Model1.performanceLog('<Random Generator>(test)',dat_test,test_mask,test_hintMask,test_hints,randomGenerator,Discriminator)    
     counter+=1    
     
-# Discriminator.save(Model1.logdir+'Models\DiscriminatorS1E{}'.format(1))
-
-#%% Run - test
-counter=0
-train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=1,repeat=5)
-test=iter(test)
-mem_Discriminator=Memorise(Discriminator,(2, 7999, 6),10)
-
-for dat_train,[mask,hintMask,hints] in tqdm(train):
-    Model1.trainWithSteps(dat_train,mask,hints,randomGenerator,mem_Discriminator,steps=False)
-    if(counter%20==0):
-        Model1.performanceLog('<Random Generator>(train)',dat_train,mask,hintMask,hints,randomGenerator,Discriminator)
-        dat_test,[test_mask,test_hintMask,test_hints]=test.next()
-        Model1.performanceLog('<Random Generator>(test)',dat_test,test_mask,test_hintMask,test_hints,randomGenerator,Discriminator)    
-    counter+=1    
-    
-
 #%% Run - Step 2
 # Then train the discriminator against a train-able generator model without unrolling
 counter=0
 model_counter=0
-train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=1,repeat=5)
+train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=5)
 test=iter(test)
+mem_Discriminator=Memorise(Discriminator,100, Data.train_batch_size,[Data.Dim,Data.Dim])
+mask_cache=Memorise(lambda x:x,100,Data.train_batch_size,[Data.Dim])
 for dat_train,[mask,hintMask,hints] in tqdm(train):
-    Model2.trainWithSteps(dat_train,mask,hints,Generator,Discriminator)
+    mask_cache(mask)
+    mask_recalled=mask_cache.recall_memory(0)
+    generated_x,x_hat=Model2.generate(Generator,dat_train,mask)
+    Model2.trainDiscriminator(dat_train,x_hat,mask_recalled,hints,mem_Discriminator)
     if(counter%20==0):
         Model2.performanceLog('<Generator>(train)',dat_train,mask,hintMask,hints,Generator,Discriminator)
         dat_test,[test_mask,test_hintMask,test_hints]=test.next()
         Model2.performanceLog('<Generator>(test)',dat_test,test_mask,test_hintMask,test_hints,Generator,Discriminator)
     counter+=1
-# Generator.save(Model1.logdir+'\Models\GeneratorS2E{}'.format(1))
-# Discriminator.save(Model1.logdir+'\Models\DiscriminatorS2E{}'.format(1))
 
-#%% Run - Step 3 (unrolled)
-# Then train the generator and discriminator with discriminator unrolling. 
+#%% Run - Step 2
+# Then train the generator with discriminator unrolling. 
+# Discriminator trained with memory replay
 counter=0
 model_counter=0
-train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=1,repeat=5)
+train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=2)
 test=iter(test)
 episodes=Model3.initialiseUnRolling(Discriminator,myDiscriminator,Data.Dim)
-for dat_train,[mask,hintMask,hints] in tqdm(train):
-    Model3.unrollDiscriminator(dat_train,mask,hints,Generator,Discriminator,leap=1)
+mem_Discriminator=Memorise(Discriminator,20, Data.train_batch_size,[Data.Dim,Data.Dim])
+mask_cache=Memorise(lambda x:x,20,Data.train_batch_size,[Data.Dim])
+for dat_train,[mask,hintMask,hints] in tqdm(train):    
+    generated_x,x_hat=Model3.generate(Generator,dat_train,mask)
+    Model3.trainDiscriminator(dat_train,x_hat,mask_cache(mask),hints,mem_Discriminator)
+    Model3.unrollDiscriminator(dat_train,mask,hints,x_hat,Discriminator)
     Model3.trainGeneratorWithDiscriminators(dat_train,mask,hints,Generator,episodes)
     if(counter%20==0):
         Model3.performanceLog('<Generator>(train)',dat_train,mask,hintMask,hints,Generator,Discriminator)
         dat_test,[test_mask,test_hintMask,test_hints]=test.next()
         Model3.performanceLog('<Generator>(test)',dat_test,test_mask,test_hintMask,test_hints,Generator,Discriminator)
     counter+=1
-# Generator.save(Model1.logdir+'\GeneratorS3E{}'.format(1))
-# Discriminator.save(Model1.logdir+'\DiscriminatorS3E{}'.format(1))
 
-
-
-
-# %%
