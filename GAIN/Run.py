6616761e33946fc0fb5f61da72ff5103de01800b
kin <kin.poon@concentra.co.uk>
2020-07-01 10:32:09 +0100

    fixing unrolling, restructure generator and GAN relation

--- GAIN/Run.py
+++ GAIN/Run.py
@@ -18,31 +18,46 @@ Data=DataModel(data_path)
 
 #%% Models
 Dim=Data.Dim
-randomGenerator=myGenerator()
 Generator=myGenerator(compositLayers([Dim*10,Dim*10,Dim*10,Dim*10,Dim*20,Dim*20,Dim*20,Dim*50,Dim*200,Dim],0))
 Discriminator=myDiscriminator(compositLayers([Dim*10,Dim*10,Dim*10,Dim*20,Dim*20,Dim*20,Dim*50,Dim*200,Dim],0))
-Model1=GAN(hyperParams={'p_miss':0.5},optimizer=tf.keras.optimizers.Adam())
+Model1=GAN(Generator,Discriminator,hyperParams={'p_miss':0.5},optimizer=tf.keras.optimizers.Adam())
+Model2=GAN(Generator,Discriminator,summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5},optimizer=tf.keras.optimizers.Adam())
+Model3=GAN(Generator,Discriminator,summary_writer=Model1.summary_writer,hyperParams={'p_miss':0.5},optimizer=tf.keras.optimizers.Adam())
 #%% Run - 1 : 
+# First train the discriminator against a random generator to increase its stability
 counter=0
-train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=200)
+train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=50)
 test=iter(test)
 for dat_train,[mask,hint_mask,hints] in tqdm(train):
-    Model1.train(dat_train,mask,hints,Generator,Discriminator,steps=5)
+    Model1.train_discriminator_with_random(dat_train,mask,hints)
     if counter%5:
-        Model1.tensorboard_log('Step 1.',dat_train,mask,hints,hint_mask,randomGenerator,Discriminator)
+        Model1.tensorboard_log_with_random('Step 1.',dat_train,mask,hints,hint_mask)
+    counter+=1
+#%% Run - 2 : 
+# First train the discriminator against a proper generator
+counter=0
+train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=10)
+test=iter(test)
+for dat_train,[mask,hint_mask,hints] in tqdm(train):
+    Model2.train_discriminator(dat_train,mask,hints,steps=5)
+    Model2.train_generator(dat_train,mask,hints)
+    if counter%5:
+        Model2.tensorboard_log('Step 2.',dat_train,mask,hints,hint_mask)
     counter+=1
     
 
-#%% Run - Step 2
-# First train the discriminator against a random generator to increase its stability
+#%% Run - Step 3
+# Training with discriminator unrolling
 counter=0
-train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=1)
+train,test=Data.getPipeLine(p_miss=0.5,p_hints=0.5,train_rate=0.8,batch_ratio=0.05,repeat=10)
 test=iter(test)
 Discriminator.intiateUnrolling(Data.Dim)
 for dat_train,[mask,hint_mask,hints] in tqdm(train):
-    Model1.train_with_unrolling(dat_train,mask,hints,Generator,Discriminator)
+    Model3.train_discriminator(dat_train,mask,hints)
+    Model3.unroll_discriminator(dat_train,mask,hints)
+    Model3.train_generator_with_discriminator_unrolled(dat_train,mask,hints)
     if counter%5:
-        Model1.tensorboard_log('Step 2.',dat_train,mask,hints,hint_mask,Generator,Discriminator)
+        Model3.tensorboard_log('Step 3.',dat_train,mask,hints,hint_mask)
     counter+=1
 
 
