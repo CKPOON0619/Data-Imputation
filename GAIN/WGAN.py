6f1fea7dc101b1f902292a32a847173dfd6fd7eb
kin <kin.poon@concentra.co.uk>
2020-07-27 10:27:18 +0100

    tensorboard logging moved to WGAN

--- GAIN/WGAN.py
+++ GAIN/WGAN.py
@@ -63,6 +63,7 @@ def get_total_generator_truth_error(data_batch,generated_data,mask):
     ## Regulating term for alteration of known truth
     return tf.reduce_sum(mask*(data_batch-generated_data)**2) / tf.reduce_sum(mask)
 
+
 #%%
 class WGAN(Orchestrator):
     '''
@@ -78,15 +79,52 @@ class WGAN(Orchestrator):
     def __init__(self,generator,critic,summary_writer=False, hyperParams={}, optimizer=tf.keras.optimizers.Adam()):
         self.critic=critic
         self.generator=generator
+        self.p_miss=0.5
         self.alpha=10
         self.episode_num=5
         super().__init__(summary_writer=summary_writer, hyperParams=hyperParams, optimizer=optimizer)
 
     def tensorboard_log(self,prefix,data_batch,mask,hints,hint_mask):
+        tau=tf.random.uniform([tf.shape(data_batch)[0],1], minval=0, maxval=1, dtype=tf.dtypes.float32, seed=None, name=None)
+        
         generated_data=self.generator.generate(data_batch,mask)
         adjusted_generated_data=mask*data_batch+generated_data*(1-mask)
-        self.critic.performance_log(self.summary_writer,prefix,data_batch,adjusted_generated_data,mask,hints,self.alpha,self.epoch)
-        self.generator.performance_log_with_critic(self.summary_writer,prefix,data_batch,mask,hints,hint_mask,self.critic.criticise,self.epoch)
+        interpolated_data=tau*adjusted_generated_data+(1-tau)*data_batch             
+  
+        lastColMask=get_test_mask(data_batch)
+        lastColHints=lastColMask+(1-lastColMask)*0.5
+        lastColMasked_sample=lastColMask*data_batch+(1-lastColMask)*tf.random.uniform(tf.shape(data_batch),minval=0,maxval=1,dtype=tf.float32)
+        generatedLastCol=self.generator.body(tf.concat(axis = 1, values = [lastColMasked_sample,lastColMask]))
+        randomLastColCritics=self.critic.criticise(lastColMasked_sample,lastColHints)
+        
+        generated_critics=self.critic.criticise(adjusted_generated_data,hints)
+        blinded_generated_genuine_critics=tf.gather_nd(generated_critics,tf.where(mask*(1-hint_mask)))
+        hinted_generated_genuine_critics=tf.gather_nd(generated_critics,tf.where(mask*(hint_mask)))
+        generated_genuine_critics=tf.gather_nd(generated_critics,tf.where(mask))
+        
+        blinded_generated_fake_critics=tf.gather_nd(generated_critics,tf.where((1-mask)*(1-hint_mask)))  
+        hinted_generated_fake_critics=tf.gather_nd(generated_critics,tf.where((1-mask)*(hint_mask)))
+        generated_fake_critics=tf.gather_nd(generated_critics,tf.where((1-mask)))
+        
+        mean_critics_diff=self.critic.calc_critic_diff_ind(data_batch,adjusted_generated_data,mask,hints,self.p_miss)
+        penalty_regulation=self.critic.calc_critic_penalty(interpolated_data,hints)
+        critic_loss=mean_critics_diff+self.alpha*penalty_regulation
+        
+        with self.summary_writer.as_default():
+            tf.summary.histogram(prefix+' generated_fake_critics',generated_fake_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' generated_genuine_critics',generated_genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' blinded_generated_fake_critics',blinded_generated_fake_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' blinded_generated_genuine_critics',blinded_generated_genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' hinted_generated_genuine_critics',hinted_generated_genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' hinted_generated_fake_critics',hinted_generated_fake_critics, step=self.epoch) 
+            tf.summary.scalar(prefix+' mean_critics_diff',mean_critics_diff, step=self.epoch) 
+            tf.summary.scalar(prefix+' penalty_regulation',penalty_regulation, step=self.epoch) 
+            tf.summary.scalar(prefix+' critic_loss',critic_loss, step=self.epoch) 
+            
+            tf.summary.histogram(prefix+' critic distribution of uniformly random last column',get_last_column(randomLastColCritics), step=self.epoch) 
+            tf.summary.histogram(prefix+' generated last column distribution',get_last_column(generatedLastCol), step=self.epoch) 
+            tf.summary.histogram(prefix+' actual last column distribution',get_last_column(data_batch), step=self.epoch) 
+            self.summary_writer.flush() 
         self.epoch.assign_add(1)
     
     @tf.function
@@ -96,17 +134,33 @@ class WGAN(Orchestrator):
         generated_data=generate_random(data_batch,mask)
         adjusted_generated_data=mask*data_batch+generated_data*(1-mask)
         interpolated_data=tau*adjusted_generated_data+(1-tau)*data_batch
+                
+        lastColMask=get_test_mask(data_batch)
+        lastColHints=lastColMask+(1-lastColMask)*0.5
+        lastColMasked_sample=lastColMask*data_batch+(1-lastColMask)*tf.random.uniform(tf.shape(data_batch),minval=0,maxval=1,dtype=tf.float32)
+        randomLastColCritics=self.critic.criticise(lastColMasked_sample,lastColHints)
         
         generated_critics=self.critic.criticise(adjusted_generated_data,hints)
-        genuine_critics=self.critic.criticise(data_batch,hints)
+        blinded_generated_genuine_critics=tf.gather_nd(generated_critics,tf.where(mask*(1-hint_mask)))
+        hinted_generated_genuine_critics=tf.gather_nd(generated_critics,tf.where(mask*(hint_mask)))
         
-        mean_critics_diff=tf.reduce_mean(generated_critics)-tf.reduce_mean(genuine_critics)
-        penalty_regulation=self.critic.calc_critic_penalty(interpolated_data,hints)    
+        generated_genuine_critics=tf.gather_nd(generated_critics,tf.where(mask))
+        blinded_generated_fake_critics=tf.gather_nd(generated_critics,tf.where((1-mask)*(1-hint_mask)))
+        hinted_generated_fake_critics=tf.gather_nd(generated_critics,tf.where((1-mask)*(hint_mask)))
+        generated_fake_critics=tf.gather_nd(generated_critics,tf.where((1-mask)))
+        mean_critics_diff=self.critic.calc_critic_diff_ind(data_batch,adjusted_generated_data,mask,hints,self.p_miss)
+        penalty_regulation=self.critic.calc_critic_penalty(interpolated_data,hints)
         critic_loss=mean_critics_diff+self.alpha*penalty_regulation
         
         with self.summary_writer.as_default():
-            tf.summary.histogram(prefix+' generated_critics',generated_critics, step=self.epoch) 
-            tf.summary.histogram(prefix+' genuine_critics',genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' generated_fake_critics',generated_fake_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' generated_genuine_critics',generated_genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' blinded_generated_fake_critics',blinded_generated_fake_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' blinded_generated_genuine_critics',blinded_generated_genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' hinted_generated_genuine_critics',hinted_generated_genuine_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' hinted_generated_fake_critics',hinted_generated_fake_critics, step=self.epoch) 
+            tf.summary.histogram(prefix+' critic distribution of uniformly random last column',get_last_column(randomLastColCritics), step=self.epoch) 
+            tf.summary.histogram(prefix+' hidden value generation errors',get_generated_value_errors(mask,hint_mask,data_batch,generated_data), step=self.epoch) 
             tf.summary.scalar(prefix+' mean_critics_diff',mean_critics_diff, step=self.epoch) 
             tf.summary.scalar(prefix+' penalty_regulation',penalty_regulation, step=self.epoch) 
             tf.summary.scalar(prefix+' critic_loss',critic_loss, step=self.epoch) 
@@ -114,7 +168,6 @@ class WGAN(Orchestrator):
         
         self.epoch.assign_add(1)
         return critic_loss
-        
         
 
     def train_critic_with_random(self,data_batch,mask,hints):
@@ -128,8 +181,9 @@ class WGAN(Orchestrator):
         '''
         generated_data=generate_random(data_batch,mask)
         adjusted_generated_data=mask*data_batch+generated_data*(1-mask)
-        self.critic.train(data_batch,adjusted_generated_data,mask,hints,self.alpha,self.optimizer)
+        self.critic.train(data_batch,adjusted_generated_data,mask,hints,self.alpha,self.p_miss,self.optimizer)
 
+    @tf.function
     def train_critic(self,data_batch,mask,hints,steps=1):
         '''
         A function that train generator and respective discriminator.
@@ -144,19 +198,15 @@ class WGAN(Orchestrator):
         for i in range(0,steps):
             generated_data=self.generator.generate(data_batch,mask)
             adjusted_generated_data=mask*data_batch+generated_data*(1-mask)
-            self.critic.train(data_batch,adjusted_generated_data,mask,hints,self.alpha,self.optimizer)
+            self.critic.train(data_batch,adjusted_generated_data,mask,hints,self.alpha,self.p_miss,self.optimizer)
         
-    def train_generator_with_critic(self,data_batch,mask,hints):
+    @tf.function
+    def train_generator(self,data_batch,mask,hints):
         with tf.GradientTape(persistent=True) as tape:
             generated_data=self.generator.generate(data_batch,mask)
             adjusted_generated_data=generated_data*(1-mask)+mask*data_batch
-            critics=self.critic.criticise(adjusted_generated_data,hints)
-            critic_loss=-tf.reduce_mean(critics)      
+            generated_critics=self.critic.criticise(adjusted_generated_data,hints)
+            critic_loss=-tf.reduce_mean(generated_critics*(1-mask))
         loss_gradients = tape.gradient(critic_loss,self.critic.body.trainable_variables)
         self.optimizer.apply_gradients(zip(loss_gradients, self.critic.body.trainable_variables))
         return critic_loss
-    
-            
-            
-
-# %%
